2499196527b964f154a9eeeb29c3eea05ab7a6f9
SteffanCrowley <steffancrow@gmail.com>
2022-05-25 10:29:33 -0700

    Create v1 of page layout

---
+++ src/modules/text.js
@@ -0,0 +1,6 @@
+function loadText() {
+  const container = document.querySelector("#content");
+  const element = document.createElement("div");
+  element.classList.add("contact");
+  element.appendChild(myMarco);
+}
