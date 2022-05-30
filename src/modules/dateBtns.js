d46d1012669d4bab75c51d3bf88b2478a3d98b82
Steffan Crowley <steffancrow@gmail.com>
2022-05-30 12:27:12 -0700

    Add 'radio' button functionality to date buttons

--- src/modules/dateBtns.js
+++ src/modules/dateBtns.js
@@ -6,14 +6,20 @@ function dateBtns() {
 
   main.classList.add("dateBtn3");
   main.innerHTML = "ALL";
+  main.setAttribute("type", "button");
+  main.setAttribute("id", "main");
   side.appendChild(main);
 
   btn.classList.add("dateBtn");
   btn.innerHTML = "Today";
+  btn.setAttribute("type", "button");
+  btn.setAttribute("id", "today");
   side.appendChild(btn);
 
   weekBtn.classList.add("dateBtn2");
   weekBtn.innerHTML = "Week";
+  weekBtn.setAttribute("type", "button");
+  weekBtn.setAttribute("id", "week");
   side.appendChild(weekBtn);
 }
 
--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -30,16 +30,37 @@ function eventListeners() {
   const dateBtn = document.querySelector(".dateBtn");
   dateBtn.addEventListener("click", (e) => {
     submitText("today");
+    dateBtn.classList.add("btn-active");
+    dateBtn2.classList.add("dateBtn2");
+    dateBtn3.classList.add("dateBtn3");
+
+    dateBtn.classList.remove("dateBtn");
+    dateBtn2.classList.remove("btn-active");
+    dateBtn3.classList.remove("btn-active");
   });
 
   const dateBtn2 = document.querySelector(".dateBtn2");
   dateBtn2.addEventListener("click", (e) => {
     submitText("week");
+    dateBtn.classList.add("dateBtn");
+    dateBtn2.classList.add("btn-active");
+    dateBtn3.classList.add("dateBtn3");
+
+    dateBtn.classList.remove("btn-active");
+    dateBtn2.classList.remove("dateBtn2");
+    dateBtn3.classList.remove("btn-active");
   });
 
   const dateBtn3 = document.querySelector(".dateBtn3");
   dateBtn3.addEventListener("click", (e) => {
     submitText("all");
+    dateBtn.classList.add("dateBtn");
+    dateBtn2.classList.add("dateBtn2");
+    dateBtn3.classList.add("btn-active");
+
+    dateBtn.classList.remove("btn-active");
+    dateBtn2.classList.remove("btn-active");
+    dateBtn3.classList.remove("dateBtn3");
   });
 }
 
